74e021d5de5cf7e9cc3658ea613dab454aa047df
youngho Kang <younghokang.1121@gmail.com>
2018-09-07 11:06:53 +0900

    테스트 클래스명 수정

--- fridge-web/src/test/java/com/poseidon/fridge/WebFoodControllerTests.java
+++ fridge-web/src/test/java/com/poseidon/fridge/WebFoodControllerTests.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
-public class WebFoodControllerTest {
+public class WebFoodControllerTests {
     
     private static ChromeDriver browser;
     

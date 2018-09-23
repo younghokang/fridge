70d252f1f3a0ed23c528b812cbbdd56dcaeafd53
youngho Kang <younghokang.1121@gmail.com>
2018-09-23 15:37:44 +0900

    : Eureka Service, Client
    Task-Url:

--- fridge-web/src/main/java/com/poseidon/Application.java
+++ fridge-web/src/main/java/com/poseidon/Application.java
@@ -3,6 +3,7 @@ package com.poseidon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
     
     @LoadBalanced
--- fridge/src/main/java/com/poseidon/FridgeApplication.java
+++ fridge/src/main/java/com/poseidon/FridgeApplication.java
@@ -2,10 +2,12 @@ package com.poseidon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class FridgeApplication {
 
 	public static void main(String[] args) {
